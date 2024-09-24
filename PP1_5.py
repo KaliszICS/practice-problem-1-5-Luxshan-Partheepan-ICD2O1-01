515fc7cc0ecc083afca3f6580f1519e5e80f1017
Luxshan-Partheepan-ICD2O1-01 <luxshan.partheepan@student.tdsb.on.ca>
2024-09-24 03:59:59 +0000

    idk

--- PP1_5.py
+++ PP1_5.py
@@ -1,18 +1,45 @@
 
 def q1():
-  #Write Assignment code here
+
+   math = input("Input an Integer: ")
+   question = int(math)
+   result = question + 3
+   print(result)
+
+
+
 
 def q2():
-  #Write Assignment code here
+  
+  moremath = input("Input a variable: ")
+  question2 = moremath + '4'
+  newidea = float(question2)
+  result = newidea + 2
+  print(result)
 
 def q3():
-  #Write Assignment code here
+  
+  geometry = input("Input a radius: ")
+  radius = float(geometry)
+  pi = 3.14
+  area = pi *(radius ** 2)
+  print(area)
+  
 
 def q4():
-  #Write Assignment code here
+
+  ideas = input("Input a number: ")
+  morestuff = float(ideas)
+  result = morestuff * 12
+  roundednumber = int(result)
+  print(roundednumber)
 
 def q5():
-  #Write Assignment code here
+  
+  number = input("Input an integer: ")
+  makingequations = int(number)
+  result = makingequations + 5
+  print("Your number is makingequations + 5 is result" )  
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
